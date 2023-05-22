135f32468befc5f9c26a1851c9bf3dfbbeb418f6
Sai Satwik <86428816+SaisatwikBiku@users.noreply.github.com>
2023-05-22 17:55:34 +0530

    Create RomanToInteger.py
    Leetcode Problem

---
+++ RomanToInteger.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def romanToInt(self, s) :
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = { "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000 }
+
+        result = 0
+
+        for i in range(len(s)) :
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]] :
+                result -= roman[s[i]]
+
+            else :
+                result += roman[s[i]]
+
+        return result            
